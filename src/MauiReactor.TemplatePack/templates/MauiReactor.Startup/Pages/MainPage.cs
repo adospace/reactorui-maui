131dca89029ad0d334edb06a8128385575c94fd7
Adolfo Marinucci <adospace@msn.com>
2022-07-05 23:48:21 +0200

    dotnet template small improvements

--- src/MauiReactor.TemplatePack/templates/MauiReactor.Startup/Pages/MainPage.cs
+++ src/MauiReactor.TemplatePack/templates/MauiReactor.Startup/Pages/MainPage.cs
@@ -7,15 +7,42 @@ using MauiReactor;
 
 namespace MauiReactor.Startup.Pages;
 
-class MainPage : Component
+class MainPageState : IState
+{
+    public int Counter { get; set; }
+}
+
+class MainPage : Component<MainPageState>
 {
     public override VisualNode Render()
     {
         return new ContentPage
         {
-            new Label("Happy coding with MauiReactor!")
+            new ScrollView
+            {
+                new VerticalStackLayout
+                {
+                    new Image("dotnet_bot.png")
+                        .HeightRequest(200)
+                        .HCenter()
+                        .Set(Microsoft.Maui.Controls.SemanticProperties.DescriptionProperty, "Cute dot net bot waving hi to you!"),
+
+                    new Label("Hello, World!")
+                        .FontSize(32)
+                        .HCenter(),
+
+                    new Label("Welcome to MauiReactor: MAUI with superpowers!")
+                        .FontSize(18)
+                        .HCenter(),
+
+                    new Button(State.Counter == 0 ? "Click me" : $"Clicked {State.Counter} times!")
+                        .OnClicked(()=>SetState(s => s.Counter ++))
+                        .HCenter()                    
+                }
                 .VCenter()
-                .HCenter()
+                .Spacing(25)
+                .Padding(30, 0)
+            }
         };
     }
 }
