6b078adca17a43f1f574f4f2cdb195232ef561dd
Adolfo Marinucci <adospace@msn.com>
2023-04-27 13:11:55 +0200

    small perf improvement to CompareUtils

--- src/MauiReactor/Internals/CompareUtils.cs
+++ src/MauiReactor/Internals/CompareUtils.cs
@@ -2,6 +2,8 @@
 {
     internal static class CompareUtils
     {
+        private const double DOUBLE_EPSILON = 0.0000001;
+
         public static bool AreEquals(object? left, object? right)
         {
             if (left == null && right == null)
@@ -17,6 +19,12 @@
                 return false;
             }
 
+            bool objectEquals = left.Equals(right);
+            if (objectEquals)
+            {
+                return true;
+            }
+
             if (left is SolidColorBrush leftSolidColorBrush &&
                 right is SolidColorBrush rightSolidColorBrush)
             {
@@ -24,26 +32,83 @@
                 return leftSolidColorBrush.Color?.ToUint() == rightSolidColorBrush.Color?.ToUint();
             }
 
-            if (left is ImageSource leftImageSource &&
-                right is ImageSource rightImageSource)
-            { 
-                return leftImageSource.ToString() == rightImageSource.ToString();
+            if (left is LinearGradientBrush leftLinearGradientBrush &&
+                right is LinearGradientBrush rightLinearGradientBrush)
+            {
+                return AreLinearGradientsClose(leftLinearGradientBrush, rightLinearGradientBrush);
             }
 
+            if (left is FileImageSource leftFileImageSource &&
+                right is FileImageSource rightFileImageSource)
+            { 
+                return leftFileImageSource.File == rightFileImageSource.File;
+            }
+
+            if (left is UriImageSource leftUriImageSource &&
+                right is UriImageSource rightUriImageSource)
+            {
+                //campare using uri equals
+                return leftUriImageSource.Uri == rightUriImageSource.Uri;
+            }
+            
             if (left is UrlWebViewSource leftUrlWebViewSource &&
                 right is UrlWebViewSource rightUrlWebViewSource)
             {
-                return leftUrlWebViewSource.Url.ToString() == rightUrlWebViewSource.Url.ToString();
+                return leftUrlWebViewSource.Url == rightUrlWebViewSource.Url;
             }
 
             if (left is double x &&
                 right is double y)
             {
-                double variance = x > y ? x - y : y - x;
-                return variance < 0.0000001;
+                return AreDoublesClose(x, y);
             }
 
-            return left.Equals(right);        
+            return false;
+        }
+
+        public static bool AreDoublesClose(double left, double right)
+        {
+            return (left > right ? left - right : right - left) < DOUBLE_EPSILON;
+        }
+
+        public static bool ArePointsClose(Point left, Point right)
+        {
+            return AreDoublesClose(left.X, right.X) && AreDoublesClose(left.Y, right.Y);
+        }
+
+        public static bool AreLinearGradientsClose(LinearGradientBrush leftLinearGradientBrush, LinearGradientBrush rightLinearGradientBrush)
+        {
+            if (leftLinearGradientBrush.IsEmpty && rightLinearGradientBrush.IsEmpty)
+            {
+                return true;
+            }
+
+            if (!ArePointsClose(leftLinearGradientBrush.StartPoint, rightLinearGradientBrush.StartPoint) ||
+                !ArePointsClose(leftLinearGradientBrush.EndPoint, rightLinearGradientBrush.EndPoint))
+            {
+                return false;
+            }
+
+            if (leftLinearGradientBrush.GradientStops.Count != 
+                rightLinearGradientBrush.GradientStops.Count)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < leftLinearGradientBrush.GradientStops.Count; i++)
+            {
+                if (!AreDoublesClose(leftLinearGradientBrush.GradientStops[i].Offset, rightLinearGradientBrush.GradientStops[i].Offset))
+                {
+                    return false;
+                }
+
+                if (leftLinearGradientBrush.GradientStops[i].Color?.ToUint() != rightLinearGradientBrush.GradientStops[i].Color?.ToUint())
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
