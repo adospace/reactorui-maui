270ffd35d21a2ef425f8f3ed69bfb72aabc652dc
Adolfo Marinucci <adospace@msn.com>
2023-05-04 12:34:42 +0200

    updated test app with inline component test page

--- samples/MauiReactor.TestApp/HomePage.cs
+++ samples/MauiReactor.TestApp/HomePage.cs
@@ -105,7 +105,10 @@ class HomePage : Component
             {
                 new Pages.CanvasAutoStackPage()
             },
-            
+            new FlyoutItem("Inline components")
+            {
+                new Pages.InlineComponentsPage()
+            },
         }
         .ItemTemplate(RenderItemTemplate);
     }
---
+++ samples/MauiReactor.TestApp/Pages/InlineComponentsPage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiReactor.TestApp.Pages;
+
+class InlineComponentsPage : Component
+{
+    public override VisualNode Render()
+    {
+        return new ContentPage
+        {
+            new VStack
+            {
+                Buttons.IncrementButton()
+            }
+            .HCenter()
+            .VCenter()
+        };
+    }
+}
+
+static class Buttons
+{
+    public static VisualNode IncrementButton()
+    {
+        return Component.Render(context =>
+        {
+            var state = context.UseState<int>();
+            return new Button(
+                state.Value == 0 ? "Click me!" : $"Counter is {state.Value}", 
+                () => state.Set(s => ++s));
+        });
+    }
+}
+
