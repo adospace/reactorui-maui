799c7bf7d7ebb7e018583579045e26ad1627afac
licon4812 <jalickolli@gmail.com>
2023-08-10 07:37:42 +1000

    created MauiReactor.AppShell template

---
+++ src/MauiReactor.TemplatePack/templates/MauiReactor.AppShell/AppShell.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MauiReactor.Pages;
+
+namespace MauiReactor
+{
+    internal class AppShell:Component
+    {
+        public override VisualNode Render()
+            => new Shell
+                {
+                    new FlyoutItem("MainPage")
+                    {
+                        new ShellContent()
+                            .Title("MainPage")
+                            .RenderContent(()=>new MainPage())
+                    },
+                    new FlyoutItem("OtherPage")
+                    {
+                        new ShellContent()
+                            .Title("OtherPage")
+                            .RenderContent(()=>new OtherPage())
+                    }
+                }
+                .ItemTemplate(RenderItemTemplate);
+
+        static VisualNode RenderItemTemplate(MauiControls.BaseShellItem item)
+            => new Grid("68", "*")
+            {
+                new Label(item.Title)
+                    .VCenter()
+                    .Margin(10,0)
+            };
+    }
+}
---
+++ src/MauiReactor.TemplatePack/templates/MauiReactor.AppShell/MauiProgram.cs
@@ -0,0 +1,32 @@
+﻿using MauiReactor;
+using MauiReactor.Pages;
+
+
+namespace MauiReactor
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiReactorApp<AppShell>(app =>
+                {
+                    app.AddResource("Resources/Styles/Colors.xaml");
+                    app.AddResource("Resources/Styles/Styles.xaml");
+
+                    app.SetWindowsSpecificAssetsDirectory("Assets");
+                })
+#if DEBUG
+            .EnableMauiReactorHotReload()
+#endif
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-SemiBold.ttf", "OpenSansSemiBold");
+                });
+
+            return builder.Build();
+        }
+    }
+}
