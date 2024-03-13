31761df5fbd8685b427ad39b1989825450fb154e
Adolfo Marinucci <adospace@msn.com>
2024-03-13 21:19:55 +0100

    Removed commented out code

--- src/MauiReactor.Maps/Map.cs
+++ src/MauiReactor.Maps/Map.cs
@@ -9,59 +9,7 @@ namespace MauiReactor.Maps;
 [ScaffoldChildren(typeof(Microsoft.Maui.Controls.Maps.Circle), nameof(Microsoft.Maui.Controls.Maps.Map.MapElements))]
 public partial class Map
 {
-    //Dictionary<string, Microsoft.Maui.Controls.Maps.Pin> _pinElementItemMap = [];
 
-    //protected override void OnAddChild(VisualNode widget, Microsoft.Maui.Controls.BindableObject childControl)
-    //{
-    //    //Validate.EnsureNotNull(NativeControl);
-    //    var thisAsIMap = (IMap)this;
-    //    if (childControl is Microsoft.Maui.Controls.Maps.Pin pinItem)
-    //    {
-    //        NativeControl.Pins.Insert(widget.ChildIndex, pinItem);
-    //        //_pinElementItemMap[pinItem.Label] = pinItem;
-    //    }
-    //    //else if (childControl is Microsoft.Maui.Controls.Maps.Polygon polygonItem)
-    //    //{
-    //    //    NativeControl.MapElements.Insert(widget.ChildIndex, polygonItem);
-    //    //    _polygonElementItemMap[childControl] = polygonItem;
-    //    //}
-    //    //else if (childControl is Microsoft.Maui.Controls.Maps.Polyline polylineItem)
-    //    //{
-    //    //    NativeControl.MapElements.Insert(widget.ChildIndex, polylineItem);
-    //    //    _polylineElementItemMap[childControl] = polylineItem;
-    //    //}
-    //    //else if (childControl is Microsoft.Maui.Controls.Maps.Circle circleItem)
-    //    //{
-    //    //    NativeControl.MapElements.Insert(widget.ChildIndex, circleItem);
-    //    //    _circleElementItemMap[childControl] = circleItem;
-    //    //}
-
-    //    base.OnAddChild(widget, childControl);
-    //}
-
-    //protected override void OnRemoveChild(VisualNode widget, Microsoft.Maui.Controls.BindableObject childControl)
-    //{
-    //    //Validate.EnsureNotNull(NativeControl);
-    //    var thisAsIMap = (IMap)this;
-    //    if (childControl is Microsoft.Maui.Controls.Maps.Pin pinItem)
-    //    {
-    //        NativeControl.Pins.Remove(pinItem);
-    //    }
-    //    //else if (_polygonElementItemMap.TryGetValue(childControl, out var polygonItem))
-    //    //{
-    //    //    NativeControl.MapElements.Remove(polygonItem);
-    //    //}
-    //    //else if (_polylineElementItemMap.TryGetValue(childControl, out var polylineItem))
-    //    //{
-    //    //    NativeControl.MapElements.Remove(polylineItem);
-    //    //}
-    //    //else if (_circleElementItemMap.TryGetValue(childControl, out var circleItem))
-    //    //{
-    //    //    NativeControl.MapElements.Remove(circleItem);
-    //    //}
-
-    //    base.OnRemoveChild(widget, childControl);
-    //}
 }
 
 [Scaffold(typeof(Microsoft.Maui.Controls.Maps.Pin))]
