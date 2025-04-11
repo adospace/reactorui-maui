ac5632f866973d686fbd83a9e7d6348d6eec292a
Adolfo Marinucci <adospace@msn.com>
2025-04-11 10:03:01 +0200

    Added ContextMenu sample in TestAppWindow demo project

--- samples/MauiReactor.TestAppWindow/Pages/MainWindow.cs
+++ samples/MauiReactor.TestAppWindow/Pages/MainWindow.cs
@@ -14,48 +14,68 @@ class MainPageState
 
 class MainWindow : Component<MainPageState>
 {
+    MauiControls.Page? _containerPage;
+
     public override VisualNode Render()
     {
-        return new Window
-        {
-            new ContentPage
-            {
-                new ScrollView
-                {
-                    new VerticalStackLayout
-                    {
-                        new Image("dotnet_bot.png")
+        return Window(
+            ContentPage(pageRef => _containerPage = pageRef,
+                ScrollView(
+                    VStack(                        
+                        Image("dotnet_bot.png")
                             .HeightRequest(200)
                             .HCenter()
                             .Set(MauiControls.SemanticProperties.DescriptionProperty, "Cute dot net bot waving hi to you!"),
 
-                        new Label("Hello, World!")
+                        Label("Hello, World!")
                             .FontSize(32)
                             .HCenter(),
 
-                        new Label("Welcome to MauiReactor: MAUI with superpowers!")
+                        Label("Welcome to MauiReactor: MAUI with superpowers!",
+                            MenuFlyout(
+                                MenuFlyoutItem("Black").OnClicked(() => OnMenuClicked("Black")),
+                                MenuFlyoutSubItem(
+                                    MenuFlyoutItem("Blue").OnClicked(() => OnMenuClicked("Black")),
+                                    MenuFlyoutItem("Green").OnClicked(() => OnMenuClicked("Green")),
+                                    MenuFlyoutItem("Red").OnClicked(() => OnMenuClicked("Red"))
+                                    )
+                                    .Text("Light"),
+                                MenuFlyoutItem("White")
+                                    .IconImageSource(new MauiControls.FontImageSource
+                                    {
+                                        Glyph = "\u23F9",
+                                        FontFamily = "Arial"
+                                    })
+                                    .OnClicked(() => OnMenuClicked("White"))
+                                )
+                            )
                             .FontSize(18)
                             .HCenter(),
 
-                        new Button(State.Counter == 0 ? "Click me" : $"Clicked {State.Counter} times!")
+                        Button(State.Counter == 0 ? "Click me" : $"Clicked {State.Counter} times!")
                             .OnClicked(()=>SetState(s => s.Counter ++))
                             .HCenter()
-                    }
+                    )
                     .VCenter()
                     .Spacing(25)
                     .Padding(30, 0)
-                }
-            }
-        }
+                )
+            )
+        )
         .Title("My Window Title")
         .OnSizeChanged(OnSizeChanged)
         ;
     }
 
+    private async Task OnMenuClicked(string color)
+    {
+        await _containerPage.DisplayAlert("Main Window", $"You clicked {color} color", "OK");
+    }
+
     private void OnSizeChanged(object sender, EventArgs args)
     {
         var window = ((MauiControls.Window)sender);
-        //System.Diagnostics.Debug.WriteLine($"Window size changed to {window.Width}x{window.Height}");
-        //System.Diagnostics.Debug.WriteLine($"Window position changed to {window.X},{window.Y}");
+        System.Diagnostics.Debug.WriteLine($"Window size changed to {window.Width}x{window.Height}");
+        System.Diagnostics.Debug.WriteLine($"Window position changed to {window.X},{window.Y}");
     }
 }
